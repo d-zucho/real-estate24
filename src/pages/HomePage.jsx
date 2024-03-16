5208b4de36075b93e18de6f01ab9f8778ea30515
D_Zucho <danny.nico.dev@outlook.com>
2024-03-15 23:17:52 -0700

    start homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import '@/styles/homepage.scss'
+
+const HomePage = () => {
+  return (
+    <div className='home-wrapper'>
+      <h1>Welcome to our page!</h1>
+      <p>Feel free to browse around and let us know if you need anything.</p>
+    </div>
+  )
+}
+
+export default HomePage
